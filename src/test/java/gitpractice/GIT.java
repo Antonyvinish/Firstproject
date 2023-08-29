1c90db668af70124e89cb15dbb46a9a2123f818e
Antonyvinish <antonyvines.av@gmail.com>
2023-08-29 12:38:39 +0530

    Update Code

--- src/test/java/gitpractice/GIT.java
+++ src/test/java/gitpractice/GIT.java
@@ -12,6 +12,15 @@ public class GIT {
 	  System.out.println("3");
         }
         
+        public void method4(){
+      	  System.out.println("4");
+              }
+        public void method5(){
+      	  System.out.println("5");
+              }
+              
+              
+        
         public static void main(String[] args) {
         	
         	GIT git= new GIT();
@@ -19,6 +28,8 @@ public class GIT {
         	git.method1();
         	git.method2();
         	git.method3();
+        	git.method4();
+        	git.method5();
 	}
 
 	
