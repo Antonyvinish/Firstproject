3d6235136ac5dcff3705563681964fe752d7bf47
Antonyvinish <antonyvines.av@gmail.com>
2023-08-29 11:15:39 +0530

    Initial codes

---
+++ src/test/java/gitpractice/GIT.java
@@ -0,0 +1,25 @@
+package gitpractice;
+
+public class GIT {
+		
+		public void method1(){
+			System.out.println("1");
+		}
+       public void method2(){
+			System.out.println("2");
+		}
+        public void method3(){
+	  System.out.println("3");
+        }
+        
+        public static void main(String[] args) {
+        	
+        	GIT git= new GIT();
+        	
+        	git.method1();
+        	git.method2();
+        	git.method3();
+	}
+
+	
+}
